f5a9684f7f68923c86bb0783ba95063c4fd94f00
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-05-30 01:08:45 +0600

    setup ready

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <h2>This is wild estates</h2>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>This is wild estates</h2>
+		</div>
+	);
 }
 
 export default App;
