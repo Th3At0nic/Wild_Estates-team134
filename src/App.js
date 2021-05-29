9b7475874e229c388237d3d198c423c0049bbe8a
Th3At0nic <islammdrahatul@gmail.com>
2021-05-29 21:38:53 +0600

    added line

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h2>This is wild estates,.hg yay</h2>
+     <h2>This is wild estates, yay boy , Im back.</h2>
     </div>
   );
 }
