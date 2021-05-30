49a3041f082f76160921776f5651768b9723f06f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-05-30 14:09:03 +0600

    added header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Components/Homepage/Homepage/Homepage';
 
 function App() {
 	return (
 		<div className="App">
-			<h2>This is wild estates</h2>
+			<Homepage></Homepage>
+			{/* <h2>This is wild estates</h2>
+			<button type="button" class="btn btn-primary">Primary</button> */}
 		</div>
 	);
 }
